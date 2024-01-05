1d1c4a3ebae4b10230a1f99e45874943ee8dbce2
Alexandre Guenot <alexandre.guenot@epita.fr>
2024-01-06 00:47:51 +0100

    fix(settings): remove useless settings

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -84,10 +84,10 @@ vim.g.loaded_netrwSettings = 1
 
 -- Apply automatically formatter
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = { "*.fish" },
-  command = "silent !fish_indent -w <afile>",
-})
+-- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+--   pattern = { "*.fish" },
+--   command = "silent !fish_indent -w <afile>",
+-- })
 
 -- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 --   pattern = { "*.py" },
@@ -105,57 +105,3 @@ P = function(v)
   print(vim.inspect(v))
   return v
 end
-
-local function popupCompileCommand()
-  -- Define the size of the floating window
-  local width = 60
-  local height = 1
-  local prompt_prefix = "> "
-
-  -- Create the prompt buffer that will be displayed
-  local buf = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_set_option(buf, "buftype", "prompt")
-  vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
-
-  -- Config the prompt
-  vim.fn.prompt_setprompt(buf, prompt_prefix)
-  vim.fn.prompt_setcallback(buf, function(input)
-    vim.opt.makeprg = input
-    vim.cmd("close!")
-  end)
-
-  -- Get the current UI
-  local ui = vim.api.nvim_list_uis()[1]
-
-  -- Create the floating window
-  local opts = {
-    relative = "editor",
-    width = width,
-    height = height,
-    col = (ui.width / 2) - (width / 2),
-    row = (ui.height / 3) - (height / 3),
-    style = "minimal",
-    border = "rounded",
-    title = { { " Compile Command ", "FloatBorder" } },
-    title_pos = "center",
-  }
-
-  -- Creating the window
-  local win = vim.api.nvim_open_win(buf, true, opts)
-  vim.cmd("startinsert")
-
-  -- Update the prompt with the current makeprg
-  vim.defer_fn(function()
-    local current_makeprg = vim.opt.makeprg:get()
-    vim.api.nvim_buf_set_text(buf, 0, #prompt_prefix, 0, #prompt_prefix, { current_makeprg })
-    vim.api.nvim_win_set_cursor(win, { 1, #prompt_prefix + #current_makeprg + 1 })
-  end, 0)
-
-  -- Useful bindings
-  vim.keymap.set("n", "<esc>", "<cmd>close!<cr>", { silent = true, buffer = buf })
-  vim.keymap.set("n", "q", "<cmd>close!<cr>", { silent = true, buffer = buf })
-end
-
---vim.keymap.set("n", "<leader>m", popupCompileCommand, {})
--- TODO: add the same with a runprg variable
--- TODO: add asynchronous jobs to run and compile (and pipe)
