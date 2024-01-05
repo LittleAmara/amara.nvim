3b6603845b46ab823b557ce1ed9b2b0459fafc10
Alexandre Guenot <alexandre.guenot@epita.fr>
2024-01-06 00:48:15 +0100

    feat(plugins: lspconfig): add temporarily lsponfig to help debugging manual lsp configurations

--- lua/plugins/tmp.lua
+++ lua/plugins/tmp.lua
@@ -1,8 +1,9 @@
+-- Only keeping it for debug, soon my version I will write my own "LspInfo"
+
 return {
   "neovim/nvim-lspconfig",
-  lazy = false,
+  cmd = "LspInfo",
   config = function()
-    require("lspconfig")
-    -- require'lspconfig'.pyright.setup{}
+    require("lspconfig.ui.windows").default_options.border = "rounded"
   end,
 }
