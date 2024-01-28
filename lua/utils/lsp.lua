f718b4813444ed2ea474af4ab5f634a807666c0f
Alexandre Guenot <alexandre.guenot@epita.fr>
2024-01-28 22:56:58 +0100

    fix(utils: lsp): remove useless notify and now format setup is on `on_attach`

--- lua/utils/lsp.lua
+++ lua/utils/lsp.lua
@@ -1,20 +1,17 @@
-local async = require("utils.async")
 local M = {}
 
 local _default_opts = {
-  on_init = function(client, _)
+  on_attach = function(client, _)
+    local bufopts = { noremap = true, silent = true, buffer = vim.api.nvim_get_current_buf() }
     if client.server_capabilities.documentFormattingProvider then
-      local bufopts = { noremap = true, silent = true, buffer = vim.api.nvim_get_current_buf() }
       vim.keymap.set("n", "<space><space>", function()
         vim.lsp.buf.format({ async = true })
       end, bufopts)
-    else
-      vim.notify(("The client %s does not support formatting"):format(client.name), vim.log.levels.WARN)
     end
   end,
 }
 
---- Setup a language server protocol in a non blocking way with |vim.schedule|
+--- Setup a language server protocol
 ---@param opts table: See |vim.lsp.start_client|
 function M.setup_lsp(opts)
   local final_opts = vim.tbl_deep_extend("force", _default_opts, opts)
