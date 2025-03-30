fc0b650f3a03c5f1999098dc6032663b84f111a2
Alexandre Guenot <alexandre.guenot@gmail.com>
2025-03-30 21:13:58 +0200

    feat(plugins): remove lspconfig as it is not needed anymore

--- lua/plugins/tmp.lua
+++
@@ -1,9 +0,0 @@
--- Only keeping it for debug, soon my version I will write my own "LspInfo"
-
-return {
-  "neovim/nvim-lspconfig",
-  cmd = "LspInfo",
-  config = function()
-    require("lspconfig.ui.windows").default_options.border = "rounded"
-  end,
-}
